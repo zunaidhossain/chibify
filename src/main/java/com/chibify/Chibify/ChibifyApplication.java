5859b2e271a346748a4800bcd486ae2c15afef07
Zunaid Hossain <zunaidhossain.india@gmail.com>
2025-11-29 01:15:30 +0530

    Imported project from start.spring.io

---
+++ src/main/java/com/chibify/Chibify/ChibifyApplication.java
@@ -0,0 +1,13 @@
+package com.chibify.Chibify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChibifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChibifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chibify/Chibify/ChibifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chibify.Chibify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChibifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
